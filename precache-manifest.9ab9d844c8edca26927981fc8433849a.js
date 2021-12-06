e8272f7b573c8e0da79b492dfcb9a1a728e0391c
irisyeung0725 <irisyeung0725@users.noreply.github.com>
2021-12-06 12:09:13 +0000

    Deploying to gh-pages from  @ f5ca3a376720a71bc11250cdbf5debba6dae01e1 🚀

---
+++ precache-manifest.9ab9d844c8edca26927981fc8433849a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0293c6eb737f55a393162c3381ebc25a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223c4f6dbd387626658a",
+    "url": "/static/css/3.a980669d.chunk.css"
+  },
+  {
+    "revision": "f2400c322543d8882bdb",
+    "url": "/static/css/4.9c4e17a0.chunk.css"
+  },
+  {
+    "revision": "80c1b2462f3a448ce88a",
+    "url": "/static/css/main.7b2e9836.chunk.css"
+  },
+  {
+    "revision": "f0de55e92ad2e99a1947",
+    "url": "/static/js/2.5ec840c5.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.5ec840c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223c4f6dbd387626658a",
+    "url": "/static/js/3.3528ef11.chunk.js"
+  },
+  {
+    "revision": "f2400c322543d8882bdb",
+    "url": "/static/js/4.3db040be.chunk.js"
+  },
+  {
+    "revision": "80c1b2462f3a448ce88a",
+    "url": "/static/js/main.036d873b.chunk.js"
+  },
+  {
+    "revision": "8fd2e529618c1700a7cd",
+    "url": "/static/js/runtime-main.60e347f7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "f402c8f6b3ff402f8e090f1f547448b9",
+    "url": "/static/media/Apache_Hive_logo.f402c8f6.svg"
+  },
+  {
+    "revision": "5741f1de3c2ddac2a89c9088a0ae8c3a",
+    "url": "/static/media/Mendix-logo.5741f1de.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a08f62bbc1ef96740d32339a03615271",
+    "url": "/static/media/Zillow-logo.a08f62bb.png"
+  },
+  {
+    "revision": "3ad77a2de2b3b89654c51e81e585351f",
+    "url": "/static/media/bloombergLogo.3ad77a2d.jpeg"
+  },
+  {
+    "revision": "cc2c3d6a5ce986fe7ef7dfb115732aab",
+    "url": "/static/media/bnuLogo.cc2c3d6a.png"
+  },
+  {
+    "revision": "4b3730a6046aae4f4be059523a47400c",
+    "url": "/static/media/boc-logo.4b3730a6.jpeg"
+  },
+  {
+    "revision": "e491865445a9cf7c4f52a4f7edae4223",
+    "url": "/static/media/contactMe.e4918654.svg"
+  },
+  {
+    "revision": "e1dd069aae01fd48cff6591d4f648457",
+    "url": "/static/media/datarobot-logo.e1dd069a.png"
+  },
+  {
+    "revision": "441b2900da76a0c8f458ddc9dbafff2a",
+    "url": "/static/media/downloading.441b2900.svg"
+  },
+  {
+    "revision": "6488e3306a3efa905c8bd650fa685928",
+    "url": "/static/media/jlptLogo.6488e330.png"
+  },
+  {
+    "revision": "05de528a0047e51c4c7d2412e9712447",
+    "url": "/static/media/stock-logo.05de528a.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "ec4e40406efc0566bd5b58ce444443b1",
+    "url": "/static/media/tripAdvisor-logo.ec4e4040.png"
+  },
+  {
+    "revision": "82f06b878998d6117f6932e175661375",
+    "url": "/static/media/visualization.82f06b87.svg"
+  },
+  {
+    "revision": "b34d22ac3239e433ef0eae6b8240c9de",
+    "url": "/static/media/working.b34d22ac.svg"
+  },
+  {
+    "revision": "a48391e4be7af698bee107ed83aaf2e4",
+    "url": "/static/media/wpi-logo-website.a48391e4.png"
+  },
+  {
+    "revision": "add21f9c7de7d846c9a5c5185c5c2aa8",
+    "url": "/static/media/wpiLogo.add21f9c.png"
+  }
+]);
